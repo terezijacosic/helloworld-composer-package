0c72716e2c487343408d2d8711df9e07c7d216a7
terezijacosic <terezija.cosic@gmail.com>
2018-04-09 13:25:56 +0200

    First commit for tena/hello-world.

---
+++ tena/hello-world/src/Helloworld/SayHello.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Helloworld;
+
+class SayHello
+{
+    public static function world()
+    {
+      return "Hello world from Tena!\n";
+    }
+}
---
+++ tena/hello-world/tests/test.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Helloworld\SayHello;
+
+echo SayHello::world();
